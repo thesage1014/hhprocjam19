f209b962372ec142b78f061d50ae39c0ef083272
kale.leopard <akasha51493@outlook.com>
2019-11-03 18:39:42 -0800

    added camera and movement scripts

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float RotateSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.Translate(0f, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+            else if (Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+        }
+    }
+
+}
