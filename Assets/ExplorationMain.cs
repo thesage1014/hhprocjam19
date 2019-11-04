2cf1539e70ac763e140dd865de1fd9565eebb36a
thesage1014 <thesage1014@gmail.com>
2019-11-03 17:05:54 -0800

    worldbehaviour

---
+++ Assets/ExplorationMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplorationMain : MonoBehaviour
+{
+    public List<Tile> tilePrefabs;
+    float lastSpawnedTime = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time-lastSpawnedTime >= 1)
+        {
+            spawnTile(0,(int)Time.time);
+        }
+        lastSpawnedTime = Time.time;
+    }
+
+    private void spawnTile(float x, float z)
+    {
+        
+    }
+}
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cuuuube : MonoBehaviour
+public class Tile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
