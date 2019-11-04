2eec336f88a45035307d64c33f1b6bca7cb6e3bc
thesage1014 <thesage1014@gmail.com>
2019-11-03 21:22:21 -0800

    ITS WORKING

--- Assets/Scripts/ExplorationMain.cs
+++ Assets/Scripts/ExplorationMain.cs
@@ -7,6 +7,8 @@ public class ExplorationMain : MonoBehaviour
     float lastSpawnedTime = 0;
     TileMap gameTiles;
     public Transform agentPos;
+    Vector2 oldPos = Vector2.zero;
+    public int scanSize = 1;
 
     void Start()
     {
@@ -16,15 +18,30 @@ public class ExplorationMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time - lastSpawnedTime >= 1) {
-            gameTiles.getTile(0,(int)Time.time);
-            lastSpawnedTime = Time.time;
+        var curPos = getCurrentPos();
+        if (!curPos.Equals(oldPos)) {
+            explore(curPos);
+            oldPos = curPos;
         }
+        //if(Time.time - lastSpawnedTime >= 1) {
+        //    gameTiles.getTile(0,(int)Time.time);
+        //    lastSpawnedTime = Time.time;
+        //}
     }
     Tile getCurrentTile() {
         return gameTiles.getTile((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
     }
     Vector2 getCurrentPos() {
+
         return new Vector2((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
+    }
+    void explore(Vector2 pos) {
+        print("exploring");
+        for(int i=-scanSize; i<scanSize; i++) {
+            for (int j = -scanSize; j < scanSize; j++) {
+                print("scanning " + i + " " + j);
+                gameTiles.getTile(i+(int)pos.x, j+(int)pos.y);
+            }
+        }
     }
 }
