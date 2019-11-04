e51393df59c311ad3ebcac458344bfb01960ed6a
thesage1014 <thesage1014@gmail.com>
2019-11-03 21:52:14 -0800

    omg sexy animations

--- Assets/Scripts/ExplorationMain.cs
+++ Assets/Scripts/ExplorationMain.cs
@@ -9,36 +9,35 @@ public class ExplorationMain : MonoBehaviour
     public Transform agentPos;
     Vector2 oldPos = Vector2.zero;
     public int scanSize = 1;
+    public bool exploring = true;
 
     void Start()
     {
         gameTiles = GetComponent<TileMap>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        var curPos = getCurrentPos();
-        if (!curPos.Equals(oldPos)) {
-            explore(curPos);
-            oldPos = curPos;
+        if(exploring) {
+            var curPos = getCurrentPos();
+            if (!curPos.Equals(oldPos)) {
+                explore(curPos);
+                oldPos = curPos;
+            }
         }
-        //if(Time.time - lastSpawnedTime >= 1) {
-        //    gameTiles.getTile(0,(int)Time.time);
-        //    lastSpawnedTime = Time.time;
-        //}
     }
+
     Tile getCurrentTile() {
-        return gameTiles.getTile((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
+        return gameTiles.getTile(Mathf.RoundToInt(agentPos.localPosition.x), Mathf.RoundToInt(agentPos.localPosition.z));
     }
     Vector2 getCurrentPos() {
 
-        return new Vector2((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
+        return new Vector2(agentPos.localPosition.x, (int)agentPos.localPosition.z);
     }
     void explore(Vector2 pos) {
         print("exploring");
-        for(int i=-scanSize; i<scanSize; i++) {
-            for (int j = -scanSize; j < scanSize; j++) {
+        for(int i=-scanSize; i<=scanSize; i++) {
+            for (int j = -scanSize; j <= scanSize; j++) {
                 print("scanning " + i + " " + j);
                 gameTiles.getTile(i+(int)pos.x, j+(int)pos.y);
             }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    List<Transform> objects;
     // Start is called before the first frame update
     void Start()
     {
-        
+        objects = new List<Transform>(GetComponentsInChildren<Transform>());
+        objects.Add(transform);
+        foreach (Transform obj in objects) {
+            obj.transform.localScale = Vector3.zero;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(Transform obj in objects) {
+            obj.transform.localScale = Vector3.Lerp(obj.transform.localScale, Vector3.one,.02f);
+        }
     }
 }
