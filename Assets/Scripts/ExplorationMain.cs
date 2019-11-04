a3b3d35d52c26f0796949e89722df5123cd23a30
thesage1014 <thesage1014@gmail.com>
2019-11-03 20:57:00 -0800

    moving stuff around

--- Assets/Scripts/ExplorationMain.cs
+++ Assets/Scripts/ExplorationMain.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ExplorationMain : MonoBehaviour
 {
     float lastSpawnedTime = 0;
-    TileMap gameTiles; 
+    TileMap gameTiles;
+    public Transform agentPos;
 
     void Start()
     {
@@ -19,5 +20,11 @@ public class ExplorationMain : MonoBehaviour
             gameTiles.getTile(0,(int)Time.time);
             lastSpawnedTime = Time.time;
         }
+    }
+    Tile getCurrentTile() {
+        return gameTiles.getTile((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
+    }
+    Vector2 getCurrentPos() {
+        return new Vector2((int)agentPos.localPosition.x, (int)agentPos.localPosition.z);
     }
 }
