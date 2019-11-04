a25d8f61f47502f9d0bb241a2caaa4eaab14cc91
thesage1014 <thesage1014@gmail.com>
2019-11-03 19:32:19 -0800

    broken

--- Assets/ExplorationMain.cs
+++ Assets/ExplorationMain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplorationMain : MonoBehaviour
 {
-    public List<Tile> tilePrefabs;
+    
     float lastSpawnedTime = 0;
     TileMap gameTiles; 
 
@@ -18,17 +18,8 @@ public class ExplorationMain : MonoBehaviour
     {
             
         if(Time.time - lastSpawnedTime >= 1) {
-            print(Time.frameCount);
-            spawnTile(0,(int)Time.time);
+            gameTiles.getTile(0,(int)Time.time);
             lastSpawnedTime = Time.time;
-            print(gameTiles.getStartingTile());
         }
-    }
-
-    private void spawnTile(int x, int z)
-    {
-        Tile newtile = Instantiate<Tile>(tilePrefabs[Random.Range(0,tilePrefabs.Count)]);
-        newtile.transform.position = new Vector3(x, 0, z);
-        gameTiles.addTile(x, z, newtile);
     }
 }
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileMap : MonoBehaviour
-{
-    
+public class TileMap : MonoBehaviour {
+    public List<Tile> tilePrefabs;
     static int xCells = 10;
     static int yCells = 10;
     Tile[,] tileMap;
@@ -18,7 +17,7 @@ public class TileMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public Tile getStartingTile()
     {
@@ -26,14 +25,22 @@ public class TileMap : MonoBehaviour
     }
     public Tile getTile(int x, int z)
     {
-
-        Tile returnTile = tileMap[x,z];
-
-        return returnTile;
+        if (x <= xCells || x < 0 || z >= yCells || z < 0)
+        {
+            Debug.LogError("Tile seach out of bounds:" + x + " " + z);
+            return null;
+        }
+        else
+        {
+            Tile returnTile = tileMap[x, z];
+            return returnTile;
+        }
     }
     // Populate new tile
-    public void addTile(int x, int z, Tile tile)
+    void addTile(int x, int z, Tile tile)
     {
+        Tile newtile = Instantiate<Tile>(tilePrefabs[Random.Range(0, tilePrefabs.Count)]);
+        newtile.transform.position = new Vector3(x, 0, z);
         tileMap[x, z] = tile;
     }
 }
